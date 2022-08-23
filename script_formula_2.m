2c2ef9e65855eb215ab9d6b00728fdf96f563ef4
Erickv123 <111807626+Erickv123@users.noreply.github.com>
2022-08-23 01:06:55 -0500

    Add files via upload

---
+++ script_formula_2.m
@@ -0,0 +1,18 @@
+clc
+clear all
+close all
+
+x = ("1/2g(V2^2-V1^2)")
+V2 = input ("Dame un dato V2 ");
+V1 = input ("Dame un dato V1 ");
+g = 9.18
+y = ("h2-h1")
+h2 = input ("Dame un dato h2 ");
+h1 = input ("Dame un dato h1 ");
+z = ("(P2-P1)/(p*g)")
+p = 1.03
+g = 9.18
+P2 = input ("Dame un dato P2 ");
+P1 = input ("Dame un dato P1 ");
+w = x+y+z
+disp("El resultado de la formula es w");
